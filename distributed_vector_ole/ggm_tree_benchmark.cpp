a0ed1338748fddb100d14fff62f5ca5a3cd60742
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2019-03-14 21:13:58 +0000

    Move benchmark to a new target

---
+++ distributed_vector_ole/ggm_tree_benchmark.cpp
@@ -0,0 +1,23 @@
+#include "benchmark/benchmark.h"
+#include "distributed_vector_ole/ggm_tree.h"
+#include "gtest/gtest.h"
+
+namespace distributed_vector_ole {
+namespace {
+
+static void BM_Expand(benchmark::State& state) {
+  std::vector<uint8_t> seed(GGMTree::kBlockSize, 42);
+  int arity = state.range(0);
+  int64_t num_leaves = state.range(1);
+  for (auto _ : state) {
+    auto tree = GGMTree::Create(arity, num_leaves, seed);
+    EXPECT_OK(tree);
+  }
+}
+BENCHMARK(BM_Expand)->Ranges({
+    {2, 1024},          // arity
+    {1 << 12, 1 << 21}  // num_leaves
+});
+
+}  // namespace
+}  // namespace distributed_vector_ole
--- distributed_vector_ole/ggm_tree_test.cpp
+++ distributed_vector_ole/ggm_tree_test.cpp
@@ -1,6 +1,5 @@
 #include "ggm_tree.h"
 #include <cmath>
-#include "benchmark/benchmark.h"
 #include "gtest/gtest.h"
 #include "mpc_utils/status_macros.h"
 
@@ -137,20 +136,6 @@ TEST_F(GGMTreeTest, GetValueInvalidNodeIndex) {
   EXPECT_EQ(value.status().code(), mpc_utils::error::INVALID_ARGUMENT);
   EXPECT_EQ(value.status().message(), "node_index out of range");
 }
-
-static void BM_Expand(benchmark::State& state) {
-  std::vector<uint8_t> seed(GGMTree::kBlockSize, 42);
-  int arity = state.range(0);
-  int64_t num_leaves = state.range(1);
-  for (auto _ : state) {
-    auto tree = GGMTree::Create(arity, num_leaves, seed);
-    EXPECT_OK(tree);
-  }
-}
-BENCHMARK(BM_Expand)->Ranges({
-    {2, 1024},          // arity
-    {1 << 12, 1 << 21}  // num_leaves
-});
 
 }  // namespace
 }  // namespace distributed_vector_ole
