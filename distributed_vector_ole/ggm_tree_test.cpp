bed5ca3d365be4e71680482c83e2414024f95f50
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2019-03-14 15:51:07 +0000

    Add failure test cases

--- distributed_vector_ole/ggm_tree_test.cpp
+++ distributed_vector_ole/ggm_tree_test.cpp
@@ -11,5 +11,29 @@ TEST(GGMTree, Constructor) {
   EXPECT_OK(tree);
 }
 
+TEST(GGMTree, ArityMustBeAtLeastTwo) {
+  std::vector<uint8_t> seed(AES_BLOCK_SIZE, 0);
+  auto tree = GGMTree::Create(1, 1<<20, seed);
+  ASSERT_FALSE(tree.ok());
+  EXPECT_EQ(tree.status().code(), mpc_utils::error::INVALID_ARGUMENT);
+  EXPECT_EQ(tree.status().message(), "arity must be at least 2");
+}
+
+TEST(GGMTree, NumberOfLeavesMustNotBeNegative) {
+  std::vector<uint8_t> seed(AES_BLOCK_SIZE, 0);
+  auto tree = GGMTree::Create(2, -1, seed);
+  ASSERT_FALSE(tree.ok());
+  EXPECT_EQ(tree.status().code(), mpc_utils::error::INVALID_ARGUMENT);
+  EXPECT_EQ(tree.status().message(), "num_leaves must not be negative");
+}
+
+TEST(GGMTree, SeedSizeMustMatchBlockSize) {
+  std::vector<uint8_t> seed(AES_BLOCK_SIZE - 1, 0);
+  auto tree = GGMTree::Create(2, 1<<20, seed);
+  ASSERT_FALSE(tree.ok());
+  EXPECT_EQ(tree.status().code(), mpc_utils::error::INVALID_ARGUMENT);
+  EXPECT_EQ(tree.status().message(), "seed must have length kBlockSize");
+}
+
 }  // namespace
 }  // namespace distributed_vector_ole
