8baa7d244d0ea6c800573c00bfb72db83dad3949
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2019-05-09 20:05:18 +0200

    Enable SPFSS and All-but-One-OT with NTL big integers

--- distributed_vector_ole/all_but_one_random_ot.h
+++ distributed_vector_ole/all_but_one_random_ot.h
@@ -2,7 +2,11 @@
 // The server and the client obtain the same random vector of lengh `N`,
 // except for the ith position, for which the client obtains nothing.
 
+#ifndef DISTRIBUTED_VECTOR_OLE_ALL_BUT_ONE_RANDOM_OT_H_
+#define DISTRIBUTED_VECTOR_OLE_ALL_BUT_ONE_RANDOM_OT_H_
+
 #include <vector>
+#include "NTL/ZZ_p.h"
 #include "absl/types/span.h"
 #include "distributed_vector_ole/ggm_tree.h"
 #include "emp-ot/emp-ot.h"
@@ -20,17 +24,21 @@ namespace all_but_one_random_ot_internal {
 // Currently, this method only truncates the leaves of `tree` and writes them to
 // `output`.
 //
-// TODO: implement packing, where each leaf of the last level actually
+// TODO: Implement packing, where each leaf of the last level actually
 //   represents multiple values of type T if sizeof(T) < sizeof(GGMTree::Block).
 //   This will require an (n-1)-out-of-n-OT on the last level.
 template <typename T>
-mpc_utils::Status UnpackLastLevel(const GGMTree& tree, absl::Span<T> output) {
+void UnpackLastLevel(const GGMTree& tree, absl::Span<T> output) {
+#pragma omp parallel for schedule(guided)
   for (int64_t i = 0; i < tree.num_leaves(); ++i) {
-    ASSIGN_OR_RETURN(GGMTree::Block leaf, tree.GetValueAtLeaf(i));
-    output[i] = static_cast<T>(leaf);
+    // ValieOrDie() is okay here as long as i stays in [0, num_leaves).
+    GGMTree::Block leaf = tree.GetValueAtLeaf(i).ValueOrDie();
+    output[i] = T(leaf);
   }
-  return mpc_utils::OkStatus();
 }
+
+// Template specialization for NTL::ZZ_p.
+void UnpackLastLevel(const GGMTree& tree, absl::Span<NTL::ZZ_p> output);
 
 }  // namespace all_but_one_random_ot_internal
 
@@ -50,9 +58,14 @@ class AllButOneRandomOT {
     if (output.empty()) {
       return mpc_utils::InvalidArgumentError("`output` must not be empty");
     }
-    ASSIGN_OR_RETURN(auto tree, ServerSendTree(output.size(), 2));
-    RETURN_IF_ERROR(
-        all_but_one_random_ot_internal::UnpackLastLevel(*tree, output));
+
+    // Pass modulus to ServerSendTree if we're operationg on ZZ_ps.
+    const NTL::ZZ* modulus = nullptr;
+    if (std::is_same<T, NTL::ZZ_p>::value) {
+      modulus = &NTL::ZZ_p::modulus();
+    }
+    ASSIGN_OR_RETURN(auto tree, ServerSendTree(output.size(), 2, modulus));
+    all_but_one_random_ot_internal::UnpackLastLevel(*tree, output);
     return mpc_utils::OkStatus();
   }
 
@@ -69,8 +82,7 @@ class AllButOneRandomOT {
       return mpc_utils::InvalidArgumentError("`index` out of range");
     }
     ASSIGN_OR_RETURN(auto tree, ClientReceiveTree(output.size(), index, 2));
-    RETURN_IF_ERROR(
-        all_but_one_random_ot_internal::UnpackLastLevel(*tree, output));
+    all_but_one_random_ot_internal::UnpackLastLevel(*tree, output);
     return mpc_utils::OkStatus();
   }
 
@@ -79,9 +91,15 @@ class AllButOneRandomOT {
       mpc_utils::comm_channel* channel,
       std::unique_ptr<mpc_utils::CommChannelEMPAdapter> channel_adapter);
 
+  // Constructs a GGMTree and obliviously sends it to the client, except for the
+  // values on the path to an index chosen by the client. If modulus is set,
+  // ensures that when the leaves are subsequently reduced modulo `modulus`, all
+  // residue classes are sampled with equal probability.
   mpc_utils::StatusOr<std::unique_ptr<GGMTree>> ServerSendTree(
-      int64_t num_leaves, int arity);
+      int64_t num_leaves, int arity, const NTL::ZZ* modulus = nullptr);
 
+  // Obliviously receives a GGMTree that is equal to the server's except at
+  // `index`.
   mpc_utils::StatusOr<std::unique_ptr<GGMTree>> ClientReceiveTree(
       int64_t num_leaves, int64_t index, int arity);
 
@@ -90,3 +108,5 @@ class AllButOneRandomOT {
   emp::SHOTExtension<mpc_utils::CommChannelEMPAdapter> ot_extension_;
 };
 }  // namespace distributed_vector_ole
+
+#endif  // DISTRIBUTED_VECTOR_OLE_ALL_BUT_ONE_RANDOM_OT_H_
